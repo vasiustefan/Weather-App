77dbd02759d363be0009cdb3a25710e16e57ca1a
vasiustefan <93321899+vasiustefan@users.noreply.github.com>
2023-03-13 18:21:25 +0200

    Card

--- WeatherApp/src/App.vue
+++ WeatherApp/src/App.vue
@@ -3,8 +3,10 @@ export default {
   data() {
     return {
       city: '',
+      date: '',
       weather: '',
-      temp: '',
+      temp_c: '',
+      feelslike_c: '',
       humidity: '',
       wind: '',
       icon: '',
@@ -18,9 +20,11 @@ export default {
       fetch(Url)
         .then((response) => response.json())
         .then((data) => {
-          this.city = data.location.name
+          this.city = data.location.name + ', ' + data.location.country
+          this.date = data.location.localtime
+          this.temp_c = data.current.temp_c + '°C'
+          this.feelslike_c = data.current.feelslike_c + '°C'
           this.weather = data.current.condition.text
-          this.temp = data.current.temp_c + '°C'
           this.humidity = data.current.humidity + '%'
           this.wind = data.current.wind_kph + 'km/h'
           this.icon = data.current.condition.icon
@@ -31,47 +35,32 @@ export default {
 </script>
 
 <template>
-  <h1>Weather App</h1>
-  <input type="text" placeholder="Enter City Name" />
-  <button @click="fetchData">Search</button>
+  <!-- <v-img src="https://imgur.com/KUoTI9m" cover></v-img> -->
+  <v-card width="300" class="mx-auto mt-5">
+    <v-card-title>Weather App</v-card-title>
+    <v-card-text>
+      <v-text-field density="compact" variant="outlined" label="Enter city name"></v-text-field>
+      <v-btn @click="fetchData" class="mt-5">Search</v-btn>
+    </v-card-text>
+  </v-card>
   <main v-if="fetchData">
-    <h5>{{ city }}</h5>
-    <p>{{ weather }}</p>
-    <p>{{ temp }}</p>
-    <p>{{ humidity }}</p>
-    <p>{{ wind }}</p>
-    <img :src="icon" />
+    <v-card width="200" class="ml-5">
+      <v-card-title>{{ city }}</v-card-title>
+      <v-card-text>
+        <v-img :src="icon" centered></v-img>
+        <p>{{ weather }}</p>
+        <p>{{ date }}</p>
+        <p>{{ temp_c }}</p>
+        <p>{{ feelslike_c }}</p>
+        <p>{{ humidity }}</p>
+        <p>{{ wind }}</p>
+      </v-card-text>
+    </v-card>
   </main>
 </template>
 
-<style>
-h1 {
-  color: blue;
-  text-align: center;
-}
-button {
-  cursor: pointer;
-}
+<style scoped>
 html {
-  background-color: lightblue;
-}
-input,
-button {
-  background-color: lightblue;
-  border-color: blue;
-  color: blue;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 10px auto;
-}
-
-main {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 10px auto;
+  background-size: cover;
 }
 </style>
--- WeatherApp/src/main.js
+++ WeatherApp/src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(vuetify).mount('#app')
