6ac01375330da2f27ddd420f869569a04143aaba
vasiustefan <93321899+vasiustefan@users.noreply.github.com>
2023-03-16 11:49:46 +0200

    v-for, c/f button

--- WeatherApp/src/App.vue
+++ WeatherApp/src/App.vue
@@ -2,11 +2,125 @@
 export default {
   data() {
     return {
+      week: [
+        {
+          id: 1,
+          weatherforecast: '',
+          iconforecast: '',
+          avghumidity: '',
+          maxwind_kph: '',
+          avgtemp_c: '',
+          avgtemp_f: ''
+        },
+        {
+          id: 2,
+          weatherforecast: '',
+          iconforecast: '',
+          avghumidity: '',
+          maxwind_kph: '',
+          avgtemp_c: '',
+          avgtemp_f: '',
+          day: ''
+        },
+        {
+          id: 3,
+          weatherforecast: '',
+          iconforecast: '',
+          avghumidity: '',
+          maxwind_kph: '',
+          avgtemp_c: '',
+          avgtemp_f: '',
+          day: ''
+        },
+        {
+          id: 4,
+          weatherforecast: '',
+          iconforecast: '',
+          avghumidity: '',
+          maxwind_kph: '',
+          avgtemp_c: '',
+          avgtemp_f: '',
+          day: ''
+        },
+        {
+          id: 5,
+          weatherforecast: '',
+          iconforecast: '',
+          avghumidity: '',
+          maxwind_kph: '',
+          avgtemp_c: '',
+          avgtemp_f: '',
+          day: ''
+        }
+      ],
+      history: [
+        {
+          id: 1,
+          weatherhistory: '',
+          iconhistory: '',
+          avgtemp_chistory: '',
+          avgtemp_fhistory: '',
+          avghumidityhistory: '',
+          maxwind_kphhistory: ''
+        },
+        {
+          id: 2,
+          weatherhistory: '',
+          iconhistory: '',
+          avgtemp_chistory: '',
+          avgtemp_fhistory: '',
+          avghumidityhistory: '',
+          maxwind_kphhistory: ''
+        },
+        {
+          id: 3,
+          weatherhistory: '',
+          iconhistory: '',
+          avgtemp_chistory: '',
+          avgtemp_fhistory: '',
+          avghumidityhistory: '',
+          maxwind_kphhistory: ''
+        },
+        {
+          id: 4,
+          weatherhistory: '',
+          iconhistory: '',
+          avgtemp_chistory: '',
+          avgtemp_fhistory: '',
+          avghumidityhistory: '',
+          maxwind_kphhistory: ''
+        },
+        {
+          id: 5,
+          weatherhistory: '',
+          iconhistory: '',
+          avgtemp_chistory: '',
+          avgtemp_fhistory: '',
+          avghumidityhistory: '',
+          maxwind_kphhistory: ''
+        },
+        {
+          id: 6,
+          weatherhistory: '',
+          iconhistory: '',
+          avgtemp_chistory: '',
+          avgtemp_fhistory: '',
+          avghumidityhistory: '',
+          maxwind_kphhistory: ''
+        },
+        {
+          id: 7,
+          weatherhistory: '',
+          iconhistory: '',
+          avgtemp_chistory: '',
+          avgtemp_fhistory: '',
+          avghumidityhistory: '',
+          maxwind_kphhistory: ''
+        }
+      ],
       city: '',
       date: '',
       weather: '',
-      weatherforecast: '',
-      weatherhistory: '',
       temp_c: '',
       temp_f: '',
       feelslike_c: '',
@@ -14,16 +128,19 @@ export default {
       humidity: '',
       wind: '',
       icon: '',
+      weatherforecast: '',
       iconforecast: '',
-      iconhistory: '',
+      avghumidity: '',
       maxwind_kph: '',
       avgtemp_c: '',
+      avgtemp_f: '',
+      weatherhistory: '',
+      iconhistory: '',
       avgtemp_chistory: '',
       avgtemp_fhistory: '',
-      avgtemp_f: '',
-      avghumidity: '',
       avghumidityhistory: '',
       maxwind_kphhistory: '',
+      isCelsius: true,
       error: ''
     }
   },
@@ -52,6 +169,7 @@ export default {
       fetch(Url)
         .then((response) => response.json())
         .then((data) => {
+          this.date = data.forecast.forecastday[0].date
           this.avgtemp_c = data.forecast.forecastday[0].day.avgtemp_c + '°C'
           this.avgtemp_f = data.forecast.forecastday[0].day.avgtemp_f + '°F'
           this.avghumidity = data.forecast.forecastday[0].day.avghumidity + '%'
@@ -62,7 +180,10 @@ export default {
     },
     fetchHistory() {
       const q = document.querySelector('input').value
-      let Url = `http://api.weatherapi.com/v1/history.json?key=dd8adb29e86c47fb896133114230903&q=${q}&dt=${this.previousDay()}&aqi=no`
+      let Url = `http://api.weatherapi.com/v1/history.json?key=dd8adb29e86c47fb896133114230903&q=${q}&dt=${this.previousDay(
+        this.date,
+        7
+      )}&aqi=no`
       fetch(Url)
         .then((response) => response.json())
         .then((data) => {
@@ -78,22 +199,10 @@ export default {
       let d = new Date(date)
       let days = ['Duminica', 'Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata']
       let dayName = days[d.getDay()]
-      let months = [
-        'Ian',
-        'Feb',
-        'Mar',
-        'Apr',
-        'Mai',
-        'Iun',
-        'Iul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Noi',
-        'Dec'
-      ]
-      let monthName = months[d.getMonth()]
-      return `${dayName} ${monthName}`
+      let day = d.getDate()
+      let month = d.getMonth() + 1
+      let year = d.getFullYear()
+      return `${dayName} ${day}.${month}.${year}`
     },
     nextDay(date, i) {
       let d = new Date(date)
@@ -103,7 +212,10 @@ export default {
     previousDay(date, i) {
       let d = new Date(date)
       d.setDate(d.getDate() - i)
-      return this.formatDate(d)
+      return d
+    },
+    toggleTemp() {
+      this.isCelsius = !this.isCelsius
     }
   }
 }
@@ -115,72 +227,42 @@ export default {
       <v-text-field density="compact" variant="outlined" label="Enter city name"></v-text-field>
       <v-btn class="ml-20" @click="fetchData" color="transparent">Search</v-btn>
     </v-card-text>
-    <v-btn class="ma-5" v-if="date" @click="fetchForecast" color="transparent">Forecast</v-btn>
-    <v-btn class="ma-5" v-if="date" @click="fetchHistory" color="transparent">History</v-btn>
   </v-card>
   <div v-if="fetchData">
-    <v-card v-if="date" width="200" class="mx-auto mt-7" color="transparent">
-      <v-card-title>{{ city }}</v-card-title>
+    <v-card v-if="date" width="250" class="mx-auto mt-7" color="transparent">
+      <v-card-title
+        >{{ city }}
+        <v-btn class="ml-1" color="transparent" @click="toggleTemp" size="small"
+          >C/F</v-btn
+        ></v-card-title
+      >
       <v-card-text>
         <v-img :src="icon" centered></v-img>
         <p>{{ weather }}</p>
         <p>{{ formatDate(date) }}</p>
-        <p>{{ date }}</p>
-        <p>{{ temp_c }} | {{ temp_f }}</p>
-        <p>{{ feelslike_c }} | {{ fellslie_f }}</p>
+        <p v-if="isCelsius == true">{{ temp_c }}</p>
+        <p v-if="isCelsius == false">{{ temp_f }}</p>
+        <p v-if="isCelsius == true">{{ feelslike_c }}</p>
+        <p v-if="isCelsius == false">{{ fellslie_f }}</p>
         <p>{{ humidity }}</p>
         <p>{{ wind }}</p>
       </v-card-text>
+      <v-btn class="ml-1 my-2" v-if="date" @click="fetchForecast" color="transparent" size="small"
+        >Forecast</v-btn
+      >
+      <v-btn class="ml-14 my-2" v-if="date" @click="fetchHistory" color="transparent" size="small"
+        >History</v-btn
+      >
     </v-card>
   </div>
   <div v-if="fetchForecast">
     <v-row>
-      <v-col cols="5" class="d-flex child-flex" centered>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
+      <v-col v-for="day in week" :key="day.id" cols="5" class="d-flex child-flex">
+        <v-card v-if="avgtemp_c" width="200" class="ml-5 mt-7" color="transparent">
           <v-card-title>{{ weatherforecast }}</v-card-title>
           <v-card-text>
             <v-img :src="iconforecast" centered></v-img>
-            <p>{{ nextDay(date, 1) }}</p>
-            <p>{{ avgtemp_c }} | {{ avgtemp_f }}</p>
-            <p>{{ avghumidity }}</p>
-            <p>{{ maxwind_kph }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherforecast }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconforecast" centered></v-img>
-            <p>{{ nextDay(date, 2) }}</p>
-            <p>{{ avgtemp_c }} | {{ avgtemp_f }}</p>
-            <p>{{ avghumidity }}</p>
-            <p>{{ maxwind_kph }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherforecast }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconforecast" centered></v-img>
-            <p>{{ nextDay(date, 3) }}</p>
-            <p>{{ avgtemp_c }} | {{ avgtemp_f }}</p>
-            <p>{{ avghumidity }}</p>
-            <p>{{ maxwind_kph }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherforecast }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconforecast" centered></v-img>
-            <p>{{ nextDay(date, 4) }}</p>
-            <p>{{ avgtemp_c }} | {{ avgtemp_f }}</p>
-            <p>{{ avghumidity }}</p>
-            <p>{{ maxwind_kph }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherforecast }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconforecast" centered></v-img>
-            <p>{{ nextDay(date, 5) }}</p>
+            <p>{{ nextDay(date, day.id) }}</p>
             <p>{{ avgtemp_c }} | {{ avgtemp_f }}</p>
             <p>{{ avghumidity }}</p>
             <p>{{ maxwind_kph }}</p>
@@ -191,72 +273,12 @@ export default {
   </div>
   <div v-if="fetchHistory">
     <v-row>
-      <v-col cols="7" class="d-flex child-flex">
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
+      <v-col v-for="day in history" :key="day.id" cols="7" class="d-flex child-flex">
+        <v-card v-if="avgtemp_chistory" width="200" class="ml-5 mt-7" color="transparent">
           <v-card-title>{{ weatherhistory }}</v-card-title>
           <v-card-text>
             <v-img :src="iconhistory" centered></v-img>
-            <p>{{ previousDay(date, 1) }}</p>
-            <p>{{ avgtemp_chistory }} | {{ avgtemp_fhistory }}</p>
-            <p>{{ avghumidityhistory }}</p>
-            <p>{{ maxwind_kphhistory }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherhistory }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconhistory" centered></v-img>
-            <p>{{ previousDay(date, 2) }}</p>
-            <p>{{ avgtemp_chistory }} | {{ avgtemp_fhistory }}</p>
-            <p>{{ avghumidityhistory }}</p>
-            <p>{{ maxwind_kphhistory }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherhistory }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconhistory" centered></v-img>
-            <p>{{ previousDay(date, 3) }}</p>
-            <p>{{ avgtemp_chistory }} | {{ avgtemp_fhistory }}</p>
-            <p>{{ avghumidityhistory }}</p>
-            <p>{{ maxwind_kphhistory }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherhistory }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconhistory" centered></v-img>
-            <p>{{ previousDay(date, 4) }}</p>
-            <p>{{ avgtemp_chistory }} | {{ avgtemp_fhistory }}</p>
-            <p>{{ avghumidityhistory }}</p>
-            <p>{{ maxwind_kphhistory }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherhistory }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconhistory" centered></v-img>
-            <p>{{ previousDay(date, 5) }}</p>
-            <p>{{ avgtemp_chistory }} | {{ avgtemp_fhistory }}</p>
-            <p>{{ avghumidityhistory }}</p>
-            <p>{{ maxwind_kphhistory }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherhistory }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconhistory" centered></v-img>
-            <p>{{ previousDay(date, 6) }}</p>
-            <p>{{ avgtemp_chistory }} | {{ avgtemp_fhistory }}</p>
-            <p>{{ avghumidityhistory }}</p>
-            <p>{{ maxwind_kphhistory }}</p>
-          </v-card-text>
-        </v-card>
-        <v-card v-if="date" width="200" class="ml-5 mt-7" color="transparent">
-          <v-card-title>{{ weatherhistory }}</v-card-title>
-          <v-card-text>
-            <v-img :src="iconhistory" centered></v-img>
-            <p>{{ previousDay(date, 7) }}</p>
+            <p>{{ previousDay(date, day.id) }}</p>
             <p>{{ avgtemp_chistory }} | {{ avgtemp_fhistory }}</p>
             <p>{{ avghumidityhistory }}</p>
             <p>{{ maxwind_kphhistory }}</p>
@@ -272,5 +294,9 @@ body {
   background-image: url('./assets/cloud.jpg');
   background-repeat: no-repeat;
   background-size: cover;
+}
+p {
+  font-size: 14px;
+  text-align: center;
 }
 </style>
